b1d588d160e042f78081f058497949c123e204f9
Vishwanath-GitHub <54012750+Vishwanath-GitHub@users.noreply.github.com>
2019-10-09 00:59:15 +0530

    Update error-handling3.go

--- Day 4/error-handling3.go
+++ Day 4/error-handling3.go
@@ -1,3 +1,26 @@
 package main
 import (
-  
+  "fmt"
+  "errors"
+  "io/ioutil"
+)
+//The errors.Wrap function returns a new error that adds context to the original error.
+func main() {
+_, err := ioutil.ReadAll(r)
+if err != nil {
+        return errors.Wrap(err, "read failed")
+  }
+switch err := errors.Cause(err).(type) {
+case *MyError:
+        // handle specifically
+default:
+        // unknown error
+}
+  cause := errors.New("whoops")
+	err := errors.Unwrap(cause) //Unwrap returns the next error in err's chain. If there is no next error, Unwrap returns nil.
+	fmt.Println(err)
+}
+//errors.Cause will recursively retrieve the topmost error which does not implement causer, which is assumed to be the original cause.
+type causer interface {
+        Cause() error
+}
